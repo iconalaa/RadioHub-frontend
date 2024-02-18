97a5d2c610af08f5a88982470fb5c40930968dc7
ayowaaab <ayoub.dahmen@esprit.tn>
2024-02-18 22:43:00 +0100

    added Settings page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,5 +23,22 @@ class HomeController extends AbstractController
     public function profile(): Response
     {
         return $this->render('home/profile.html.twig', []);
+    }
+
+    #[Route('/settings/{id}', name: 'app_settings')]
+    public function settingsUser($id, UserRepository $user, ManagerRegistry $managerRegistry, Request $req): Response
+    {
+        $em = $managerRegistry->getManager();
+        $dataid = $user->find($id);
+        $form = $this->createForm(UserType::class, $dataid);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $em->persist($dataid);
+            $em->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+        return $this->renderForm('home/settings.html.twig', [
+            'f' => $form
+        ]);
     }
 }
